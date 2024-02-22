32dc0a26891198a26cdb061e69e33082d2e939a0
Ziqi Wang <wangziqi2385823041@outlook.com>
2024-02-22 22:19:12 +0200

    complete 3.4

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -50,6 +50,20 @@ app.get("/api/persons/:id", (request, response) => {
     }
 })
 
+app.delete("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id);
+    const person = info.find(person => person.id === id)
+    if (person) {
+        info = info.filter(person => person.id != id)
+        response.json(person)
+    } else {
+        response.send("<h1>Not Found</h1>")
+        response.status(404).end()
+    }
+    
+    
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
