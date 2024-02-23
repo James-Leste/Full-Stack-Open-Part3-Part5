cefc29d69cb4ae3bca5a41424e077cfb117d31d0
Ziqi Wang <wangziqi2385823041@outlook.com>
2024-02-23 13:54:05 +0200

    complete 3.13 - 3.14

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require("./models/persons.js")
 
 const app = express()
 
@@ -8,6 +9,7 @@ app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 
+//morgan logging config
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(function (tokens, req, res) {
     return [
@@ -20,78 +22,56 @@ app.use(morgan(function (tokens, req, res) {
     ].join(' ')
   }))
 
-let info = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
-
+//get all persons
 app.get("/api/persons", (request, response) => {
-    response.json(info)
+ Person.find({}).then(result => {
+        response.json(result)
+    })
 })
 
-app.get("/info", (request, response) => {
-    const length = info.length
-    response.send(`
-        <p>Phonebook has info for ${length} people</p>
-        <p>${Date(Date.now())}</p>
-        `)
-})
+// app.get("/info", (request, response) => {
+//     const length = info.length
+//     response.send(`
+//         <p>Phonebook has info for ${length} people</p>
+//         <p>${Date(Date.now())}</p>
+//         `)
+// })
 
 app.get("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id);
-    const person = info.find(person => person.id === id)
-    if (person) {
-        response.json(person)
-    } else {
-        response.send("<h1>Not Found</h1>")
+    const id = request.params.id
+ Person.find({id:id}).then(result => {
+        response.json(result[0])
+    }).catch(error => {
+        response.send(`<h1>${error}</h1>`)
         response.status(404).end()
-    }
+    })
 })
 
-app.delete("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id);
-    const person = info.find(person => person.id === id)
-    if (person) {
-        info = info.filter(person => person.id != id)
-        response.json(person)
-    } else {
-        response.send("<h1>Not Found</h1>")
-        response.status(404).end()
-    }
-})
+// app.delete("/api/persons/:id", (request, response) => {
+//     const id = Number(request.params.id);
+//     const person = info.find(person => person.id === id)
+//     if (person) {
+//         info = info.filter(person => person.id != id)
+//         response.json(person)
+//     } else {
+//         response.send("<h1>Not Found</h1>")
+//         response.status(404).end()
+//     }
+// })
 
 app.post("/api/persons", (request, response) => {
     let person = request.body
     if (!Object.hasOwn(person, "name")) {
         //console.log('%c [ person ]-68', 'font-size:13px; background:pink; color:#bf2c9f;', person)
         response.json({ error: 'No name attribute' }).status(400).end()
-    } else if (info.find(p => p.name === person.name)) {
-        //console.log('%c [ person ]-68', 'font-size:13px; background:pink; color:#bf2c9f;', person)
-        response.json({ error: 'name must be unique' }).status(400).end()
     } else {
         const uuid = crypto.randomUUID()
         //console.log('%c [ uuid ]-66', 'font-size:13px; background:pink; color:#bf2c9f;', uuid)
         person = { id: uuid, ...person }
-
-        response.json(person)
+        new Person(person).save().then(result => {
+            console.log(`added ${person.name} number ${person.number} to phonebook`)
+            response.json(person)
+        })
     }
 
 })
---
+++ part3/phonebook_backend/models/persons.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch(error => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
+
+const phonebookSchema = new mongoose.Schema({
+    id: String,
+    name: String,
+    number: String,
+})
+
+phonebookSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+const Person = mongoose.model('Person', phonebookSchema)
+
+module.exports = Person
+
+// if (process.argv.length === 3) {
+//     Person.find({}).then(result => {
+//         result.forEach(note => {
+//             console.log('%c [ note ]-33', 'font-size:13px; background:pink; color:#bf2c9f;', typeof (note))
+
+//             console.log(note)
+//         })
+//         mongoose.connection.close()
+//     })
+// } else if (process.argv.length == 5) {
+//     const name = process.argv[3]
+//     const number = process.argv[4]
+//     const person = new Person({
+//         id: crypto.randomUUID(),
+//         name: name,
+//         number: number
+//     })
+
+//     person.save().then(result => {
+//         console.log(`added ${name} number ${number} to phonebook`)
+
+//     })
+// } else {
+//     mongoose.connection.close()
+// }
+
+
+
+
