ee61453767f1cb2c086899783257fb7faf66b1fb
Ziqi Wang <wangziqi2385823041@outlook.com>
2024-03-04 21:10:23 +0200

    complete 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (list) => {
+    console.log('%c [ list ]-6', 'font-size:13px; background:pink; color:#bf2c9f;', list)
+    const result = list.reduce((acc, cur) => acc + cur.likes, 0)
+    return list.length === 0 ? 0 : result
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
