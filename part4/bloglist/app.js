6abfd5da2c0678f74fa7e3746311f31e20becd81
Ziqi Wang <wangziqi2385823041@outlook.com>
2024-03-20 13:24:06 +0200

    add module and middleware in app.js

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+require('express-async-errors')
 const app = express()
 // enable .env
 require('dotenv').config()
@@ -6,15 +7,23 @@ require('dotenv').config()
 const cors = require('cors')
 //enable logging
 const morgan = require('morgan')
+
 //database mode
 const blogRouter = require('./controllers/blogController')
 const usersRouter = require('./controllers/userController')
+const authRouter = require('./controllers/authController')
+
+const middleware = require('./utils/middleware')
+
 
 
 app.use(cors())
 app.use(express.json())
-app.use(blogRouter)
+app.use('/api/blogs',blogRouter)
 app.use(usersRouter)
+app.use('/api/login', authRouter)
+app.use(middleware.errorHandler)
+
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(function (tokens, req, res) {
